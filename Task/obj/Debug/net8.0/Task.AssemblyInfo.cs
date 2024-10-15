55c8a7e3a769ff255aff3e6332516f1fbcc1d0b4
Alexandr <zhk.aleksandr@gmail.com>
2024-10-15 19:02:38 +0300

    Deleted old program

--- Task/obj/Debug/net8.0/Task.AssemblyInfo.cs
+++ Task/obj/Debug/net8.0/Task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+703103bebc714f4cfedd983be413f1bc67c169c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b914448ae65550ed15eaa536332796f3f3fc5244")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
